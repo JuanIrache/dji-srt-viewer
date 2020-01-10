ae5873453dd9a69bee4f221a5c0d213df1d0cb68
JuanIrache <yrache@gmail.com>
2020-01-10 21:44:46 +0100

    enable async code

--- build.js
+++ build.js
@@ -1,8 +1,11 @@
-var fs = require("fs");
-var browserify = require("browserify");
-browserify("./sketch.js")
-  .transform("babelify", {presets: ["env"],
-	global: true,
-  ignore: /\/node_modules\/(?!(dji_srt_parser|latlon_to_xy|p5_gui|mapbox_static_helper)\/)/})
+var fs = require('fs');
+var browserify = require('browserify');
+browserify('./sketch.js')
+  .transform('babelify', {
+    presets: ['env'],
+    plugins: ['transform-async-to-generator', 'transform-runtime'],
+    global: true,
+    ignore: /\/node_modules\/(?!(dji_srt_parser|latlon_to_xy|p5_gui|mapbox_static_helper)\/)/
+  })
   .bundle()
-  .pipe(fs.createWriteStream("bundle.js"));
+  .pipe(fs.createWriteStream('bundle.js'));
