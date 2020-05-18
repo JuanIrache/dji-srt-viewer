fcbe0f8c0edc732168c5f26ad8413ca1e8ef6095
JuanIrache <yrache@gmail.com>
2020-05-18 16:56:45 +0200

    update build process

--- build.js
+++ build.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
 var browserify = require('browserify');
-browserify('./sketch.js')
-  .transform('babelify', {
-    presets: ['env'],
-    plugins: ['transform-async-to-generator', 'transform-runtime'],
-    global: true,
-    ignore: /\/node_modules\/(?!(dji_srt_parser|latlon_to_xy|p5_gui|mapbox_static_helper)\/)/
-  })
+var babelify = require('babelify');
+
+browserify()
+  .transform(babelify)
+  .require('./sketch.js', { entry: true })
   .bundle()
+  .on('error', function (err) {
+    console.log('Error: ' + err.message);
+  })
   .pipe(fs.createWriteStream('bundle.js'));
