b7d684f5e114c54c01f9cf0eae527ce82b9c9439
JuanIrache <yrache@gmail.com>
2020-05-18 19:13:49 +0200

    fix babel configuration

--- build.js
+++ build.js
@@ -5,6 +5,10 @@ var babelify = require('babelify');
 browserify()
   .transform(babelify, {
     presets: ['@babel/preset-env'],
+    plugins: [
+      '@babel/plugin-transform-async-to-generator',
+      '@babel/plugin-transform-runtime'
+    ],
     global: true
   })
   .require('./sketch.js', { entry: true })
