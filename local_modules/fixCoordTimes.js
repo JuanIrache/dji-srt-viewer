15d722b17f05457a31f13512dd77815d268a0dc1
Juan Irache <yrache@gmail.com>
2022-01-17 19:00:02 +0100

    add missing file

---
+++ local_modules/fixCoordTimes.js
@@ -0,0 +1,27 @@
+const fixCoordTimes = converted => {
+  var lastCoordsTime;
+
+  if (converted && converted.features && converted.features.length) {
+    for (var i = 0; i < converted.features.length; i++) {
+      var feature = converted.features[i];
+      if (feature.properties && feature.properties.coordTimes) {
+        var coordTimes = feature.properties.coordTimes;
+        if (coordTimes.length) {
+          for (var j = 0; j < coordTimes.length; j++) {
+            if (
+              lastCoordsTime &&
+              lastCoordsTime >= new Date(coordTimes[j]).getTime()
+            ) {
+              coordTimes[j] = new Date(lastCoordsTime + 1000).toISOString();
+            }
+            lastCoordsTime = new Date(coordTimes[j]).getTime();
+          }
+        }
+      }
+    }
+  }
+
+  return converted;
+};
+
+module.exports = fixCoordTimes;
